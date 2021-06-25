07e8292a08b532a780d84170d732eac24868ccaa
SeungJinLee <seungjin9777@gmail.com>
2021-06-25 19:06:46 +0900

    add - 프로젝트 기본 디렉토리 생성

---
+++ src/main/java/com/strongdealer/mobile/MobileApplication.java
@@ -0,0 +1,13 @@
+package com.strongdealer.mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileApplication.class, args);
+	}
+
+}
